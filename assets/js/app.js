fe0bc09069c75b4335bdbe409194f6988ad97c2a
pamcuellas <pamcuellas@gmail.com>
2019-10-22 00:18:45 -0400

    added menu colapse forced.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,14 @@ $(".portfolio").click( function (index) {
   $(".navbar-collapse").collapse('hide');
 });
 
+// Force colapse hide when a menu link is clicked 
+$( ".dropdown-item" ).click( function (index) {
+  $(".navbar-collapse").collapse('hide');
+});
+$( ".nav-item" ).click( function (index) {
+  $(".navbar-collapse").collapse('hide');
+});
+
 /*  TOOLTIP  */
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();   
